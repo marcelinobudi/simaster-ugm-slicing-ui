5beb95d91ffafb4b8807bc427905a717de089cca
marcelinobudi <marcelinobprakasya@gmail.com>
2026-03-02 20:12:41 +0700

    Add image for empty schedule

--- lib/widgets/your_schedule.dart
+++ lib/widgets/your_schedule.dart
@@ -6,8 +6,24 @@ class YourSchedule extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Column(
-      crossAxisAlignment: CrossAxisAlignment.end,
-      children: [Text('Jadwal Anda')],
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text('Jadwal Anda'),
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Image.asset(
+              'assets/images/your_schedule_empty.jpeg',
+              width: 200,
+              height: 200,
+            ),
+            Text(
+              'Jadwal hari ini kosong, mari kita\nmaksimalkan waktu yang ada...',
+              textAlign: TextAlign.center,
+            ),
+          ],
+        ),
+      ],
     );
   }
 }
